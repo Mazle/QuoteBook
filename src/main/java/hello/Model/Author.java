b214c1bae53f32393fd38ad15f784d7dc7768e99
Alexey <Switch_h13@mail.ru>
2018-10-10 22:09:33 +0400

    not working.HIBERNATE.Making Entities

--- src/main/java/hello/Model/Author.java
+++ src/main/java/hello/Model/Author.java
@@ -1,6 +1,40 @@
 package hello.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private long id;
-    private String NickName;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    public Author() {
+    }
+
+    public Author(long id, String nickName) {
+        this.id = id;
+        nickName = nickName;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        nickName = nickName;
+    }
 }
--- src/main/java/hello/Model/Quote.java
+++ src/main/java/hello/Model/Quote.java
@@ -1,13 +1,67 @@
 package hello.Model;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
+//todo: QUESTION: ужно ли в аннтоациях к полям указывать nullable и unique там, где они прописаны в бд?
 
-
+@Entity
+@Table(name = "quotes")
 public class Quote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private long id;
+    @Column(name = "content")
     private String content;
+    @Column(name = "date")
     private LocalDate date;
+    @ManyToOne
+    @JoinColumn(name = "id")
     private Author author;
+
+    public Quote() {
+    }
+
+    public Quote(long id, String content, LocalDate date, Author author) {
+        this.id = id;
+        this.content = content;
+        this.date = date;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+
 
 }
